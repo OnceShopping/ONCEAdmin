fa168b2240a3e8243d8efbf7dc9f365ea360517b
ssid1771 <ssid1771@gmail.com>
2017-11-09 19:43:22 +0900

    20171109/한유경/manager back 일부

--- ONCEAdmin/src/main/java/once/manager/service/ManagerService.java
+++ ONCEAdmin/src/main/java/once/manager/service/ManagerService.java
@@ -1,5 +1,11 @@
 package once.manager.service;
 
+import once.manager.vo.ManagerVO;
+
 public interface ManagerService {
+
+	boolean checkPassword(String managerId, String password);
+
+	ManagerVO selectById(String managerId);
 
 }
