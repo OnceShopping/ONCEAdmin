8df06c2a3aabb647a33e71f46c2bc06e583b440e
yeojin4465 <yeojin4465@naver.com>
2017-11-10 15:30:24 +0900

    20171110/yeojin/고객 관리 1차 구성

--- ONCEAdmin/src/main/java/once/customer/dao/CustomerDAOImpl.java
+++ ONCEAdmin/src/main/java/once/customer/dao/CustomerDAOImpl.java
@@ -1,12 +1,26 @@
 package once.customer.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import once.customer.vo.CustomerVO;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+	
+	
+	@Override
+	public List<CustomerVO> selectAll() {
+		
+		List<CustomerVO> list = sqlSession.selectList("once.customer.dao.CustomerDAO.selectAll");
+		
+		return list;
+	}
+	
 }
