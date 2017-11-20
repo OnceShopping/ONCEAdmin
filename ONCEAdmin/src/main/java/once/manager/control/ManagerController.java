07889355a1c0323b32d657c02771e4e1595454eb
JIKYUNGHEE <32975808+JIKYUNGHEE@users.noreply.github.com>
2017-11-20 10:23:41 +0900

    20171120/지경희/LoginController 병합
    LoginController 병합

--- ONCEAdmin/src/main/java/once/manager/control/ManagerController.java
+++ ONCEAdmin/src/main/java/once/manager/control/ManagerController.java
@@ -30,6 +30,43 @@ public class ManagerController {
 	@Autowired
 	private ManagerService service;
 	
+	@Autowired
+	private NoticeService nService;
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(@RequestParam("id")String id, @RequestParam("password")String password, Model model) {
+		
+		ManagerVO manager = new ManagerVO();
+		manager.setManagerId(id);
+		manager.setPassword(password);
+		
+		ManagerVO loginVO = service.login(manager);
+				
+		if(loginVO==null) {
+			model.addAttribute("message", "Please check your ID or Password");
+			
+			return "login/loginFail";
+		}else {
+			model.addAttribute("loginVO", loginVO);
+			System.out.println(loginVO);
+			if(loginVO.getType().equals("admin")) {
+				List<NoticeVO> list = nService.selectAllNotice();
+				model.addAttribute("list", list);
+				return "admin/notice/list";
+			}else if(loginVO.getType().equals("infoManger")) {
+				
+				return "infoManager/itemManage/addItem";
+			}else if(loginVO.getType().equals("storeManager")) {
+				
+				return "storeManager/itemManage/registerItem";
+			}else {
+				model.addAttribute("message", "type이 이상합니다");
+				
+				return "login/loginFail";
+			}
+		}
+	}
+	
 	//패스워드 체크 페이지
 	@RequestMapping(value = "/check", method = RequestMethod.GET)
 	public String checkForm() {
