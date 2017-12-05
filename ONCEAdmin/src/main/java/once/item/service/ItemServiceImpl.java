f4899f7645935148ad37710d6bdae8a7bbd530a9
yeojin4465 <yeojin4465@naver.com>
2017-12-05 15:43:45 +0900

    20171205/yeojin/상품 등록 페이지, 상품 등록 후 표시되는 모달 다이얼로그 페이지, 상품 리스트 페이지 작업 중

--- ONCEAdmin/src/main/java/once/item/service/ItemServiceImpl.java
+++ ONCEAdmin/src/main/java/once/item/service/ItemServiceImpl.java
@@ -1,14 +1,96 @@
 package once.item.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import once.item.dao.ItemDAO;
+import once.item.vo.ItemContentsVO;
+import once.item.vo.ItemImgVO;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 	
 	@Autowired
 	private ItemDAO dao;
+		
+	//상품 등록을 위한 매장 찾기
+	@Override
+	public ItemContentsVO findStore(String id) {	
+		return dao.findStore(id);
+	}
 	
+	//item 테이블에 상품 등록
+	@Override
+	public void addItem(ItemContentsVO item) {
+		dao.addItem(item);
+	}
+	
+	//item 테이블에 detail 등록
+	@Override
+	public void updateDetail(ItemContentsVO item) {
+		dao.updateDetail(item);
+	}
+	
+	//itemColor, imgTable을 위한 F.K(item 테이블의 num) 찾기
+	@Override
+	public int findNum() {	
+		return dao.findNum();
+	}
+	
+	//itemColor 테이블에 상품 등록
+	@Override
+	public void addItemColor(ItemContentsVO item) {
+		dao.addItemColor(item);
+	}
+	
+	//itemDetail 테이블에 상품 등록
+	@Override
+	public void addItemDetail(ItemContentsVO item) {
+		dao.addItemDetail(item);
+	}
+	
+	//imgTable 테이블에 상품 등록
+	@Override
+	public void addImage(ItemImgVO itemImg) {
+		dao.addImage(itemImg);
+	}
+	
+	//매장의 등록된 상품 리스트 출력
+	@Override
+	public List<ItemContentsVO> selectAll(String storeName) {
+		List<ItemContentsVO> list = dao.selectAll(storeName);
+		return list;
+	}
+	
+	//상품 코드 기존 DB에 존재하는지 여부 확인	
+	@Override
+	public boolean checkItemNo(String itemNo) {		
+		return dao.checkItemNo(itemNo);
+	}
+	
+	//선택한 size 삭제
+	@Override
+	public void deleteSize(String size) {
+		dao.deleteSize(size);
+	}
+	
+	//추가하려는 size가 기존 DB에 존재하는지 여부 확인
+	@Override
+	public boolean checkSize(ItemContentsVO item) {
+		return dao.checkSize(item);
+	}
+	
+	//item 검색
+	@Override
+	public List<ItemContentsVO> search(ItemContentsVO item) {
+		return dao.search(item);
+	}
+	
+	//item list 페이징
+	@Override
+	public List<ItemContentsVO> selectPage(List page) {
+		return dao.selectPage(page);
+	}
 }
---
+++ ONCEAdmin/src/main/webapp/WEB-INF/jsp/storeManager/itemManage/again.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- <title>Insert title here</title> -->
+<link rel="stylesheet" href="${pageContext.request.contextPath }/resources/css/bootstrap.css">
+<script src="${pageContext.request.contextPath }/resources/js/jquery.min.js"></script>
+<script	src="${pageContext.request.contextPath }/resources/js/bootstrap.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+	
+	var changePage;
+	$('#exampleModal').modal('show');
+	
+});
+
+</script>
+</head>
+<body>
+<body class="">
+<!-- Modal --> 						
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">
+						<span aria-hidden="true">×</span>
+						<span class="sr-only">Close</span>
+						</button>
+					<h3 class="modal-title" id="exampleModalLabel">REGISTER COMPLETE!</h3>
+			 	</div>
+		 	<div class="modal-body">
+		 		<div id="ask">상품 등록이 완료되었습니다.<br/>계속 상품을 등록하시겠습니까?</div>
+			</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary" data-dismiss="modal" id="yes" onclick="location.href='${pageContext.request.contextPath }/item/register'">YES</button>
+					<button type="button" class="btn btn-primary" data-dismiss="modal" id="no" onclick="location.href='${pageContext.request.contextPath }/item/list'">NO</button>
+				</div>
+			</div>
+		 </div>
+	</div>
+</body>
+</html>
